aa4e72bb8a2d3a3bead2b824348e5fd988a0879c
Paweł Orzech <pawel@pawelorzech.pl>
2026-03-27 10:18:39 +0100

    feat: project setup with config and dependencies

---
+++ app/config.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+import os
+from cryptography.fernet import Fernet
+
+
+TORN_API_KEY: str = os.environ.get("TORN_API_KEY", "")
+FACTION_ID: int = int(os.environ.get("FACTION_ID", "11559"))
+CACHE_TTL: int = int(os.environ.get("CACHE_TTL", "60"))
+
+_enc_key = os.environ.get("ENCRYPTION_KEY")
+if not _enc_key:
+    _enc_key = Fernet.generate_key().decode()
+    print("WARNING: No ENCRYPTION_KEY set. Generated ephemeral key. Keys will be lost on restart.")
+
+ENCRYPTION_KEY: str = _enc_key
